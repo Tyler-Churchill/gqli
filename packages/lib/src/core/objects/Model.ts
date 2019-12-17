251aea3fcd7b3abeae01ab451abf474d7a2061a5
Tyler Churchill <ctyler@live.com>
2019-12-16 23:49:51 -0700

    chore: remove unecessary core files

--- packages/lib/src/core/objects/Model.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectOptions } from 'type-graphql/dist/decorators/ObjectType';
-
-export declare function Model(): ClassDecorator;
-export declare function Model(options: ObjectOptions): ClassDecorator;
-export declare function Model(
-  name: string,
-  options?: ObjectOptions,
-): ClassDecorator;
